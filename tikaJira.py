44710db9bcbc47a4b121aa116c1901d539999eeb
Yiupang <leopangchan@gmail.com>
2017-08-07 12:56:38 -0700

    refactor codes

--- tikaJira.py
+++ tikaJira.py
@@ -1,12 +1,6 @@
 from jira import JIRA
 import re
 
-options = {
-	'server': 'https://issues.apache.org/jira'
-}
-
-jira = JIRA(options)
-allTIKAIssue = jira.search_issues("project=TIKA")
 
 '''
 Statuses:
@@ -21,12 +15,42 @@ Statuses:
 Goal: To resolve this question:
 	how many requirements are already defined in Jira but their implementation has not started yet.
 '''
-def getOpenFeature():
-	unimplementedIssue = []
-	for issue in allTIKAIssue:
-		if str(issue.fields.status) == 'Open':
-			unimplementedIssue.append(issue)
-	return unimplementedIssue
+def getOpenFeatures(jira):
+	return jira.search_issues("project=TIKA and issueType=\'New Feature\' and status=\'Open\'", maxResults=1000)
 
-print (len(allTIKAIssue))
-print (getOpenFeature())
+'''
+Goal: To resolve this question:
+		for each requirement in Tika, compute how many requirements have been implemented (resolved or closed) 
+	before this requirement started to be implemented (i.e., until it became in porgress).
+'''
+def getNumRequirementsBeforeThis(jira, reqName):
+	# 1. Get requirements until this requriement order by the completedTime.
+	# 2. Count the number of the requirements.
+	return None
+
+'''
+Goal: To resolve this question:
+	how many requirement have started to be implemented but are not completely implemented.
+'''
+def getOpenInProgressFeatures(jira):
+	return jira.search_issues("project=TIKA and issueType=\'New Feature\' and status=\'In Progress\'", maxResults=1000)
+
+'''
+Goal: To resolve this question:
+	number of commits for that requirement.
+'''
+def getCommitForARequirement(jira, reqName):
+
+	return 0
+
+def main():
+	jira = JIRA({
+		'server': 'https://issues.apache.org/jira'
+	})
+
+	print ("The number of open requirements: ", len(getOpenFeatures(jira)))
+	print ("The number of requirements in progress: ", len(getOpenInProgressFeatures(jira)))
+	# testing: print (jira.issue("TIKA-2232").fields.assignee)
+
+if __name__ == "__main__":
+	main()
