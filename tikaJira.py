28048164a236d164d22ea288995fb3acfb839c59
Yiupang <leopangchan@gmail.com>
2017-08-08 00:13:15 -0700

    implemented PRECHANGE-5

--- tikaJira.py
+++ tikaJira.py
@@ -1,7 +1,9 @@
 from jira import JIRA
 import re
 
-
+MAX_RESULTS = 1000
+jira = None
+TIKA_REQ_STR = "project=TIKA and issueType=\'New Feature\' AND "
 '''
 Statuses:
 1. OPEN
@@ -16,7 +18,7 @@ Goal: To resolve this question:
 	how many requirements are already defined in Jira but their implementation has not started yet.
 '''
 def getOpenFeatures(jira):
-	return jira.search_issues("project=TIKA and issueType=\'New Feature\' and status=\'Open\'", maxResults=1000)
+	return jira.search_issues(TIKA_REQ_STR + "status=\'Open\'", maxResults=MAX_RESULTS)
 
 '''
 Goal: To resolve this question:
@@ -24,16 +26,27 @@ Goal: To resolve this question:
 	before this requirement started to be implemented (i.e., until it became in porgress).
 '''
 def getNumRequirementsBeforeThis(jira, reqName):
-	# 1. Get requirements until this requriement order by the completedTime.
-	# 2. Count the number of the requirements.
-	return None
+	startProgressDate = None
+	# 1. get the time where the issue is changed to in progress
+	issue = jira.issue(reqName, expand='changelog')
+	changelog = issue.changelog
+	for history in changelog.histories:
+		for item in history.items:
+			if item.field == 'status' and item.fromString == 'Open' and item.toString == 'In Progress':
+				print ('[DEBUG] Date:' + history.created + ' From:' + item.fromString + ' To:' + item.toString)
+				startProgressDate = re.findall('(\d{4}-\d{2}-\d{2})', history.created)[0]
+
+	# 2. all the issues before that time with type of New Feature and status of RESOLVED or CLOSED
+	allIssueBeforeThis = jira.search_issues(TIKA_REQ_STR + "status WAS IN (\'Resolved\', \'Closed\') BEFORE " + startProgressDate, maxResults=MAX_RESULTS)
+	numIssueBeforeThis = len(allIssueBeforeThis)
+	return numIssueBeforeThis
 
 '''
 Goal: To resolve this question:
 	how many requirement have started to be implemented but are not completely implemented.
 '''
 def getOpenInProgressFeatures(jira):
-	return jira.search_issues("project=TIKA and issueType=\'New Feature\' and status=\'In Progress\'", maxResults=1000)
+	return jira.search_issues(TIKA_REQ_STR + "status=\'In Progress\'", maxResults=MAX_RESULTS)
 
 '''
 Goal: To resolve this question:
@@ -50,7 +63,12 @@ def main():
 
 	print ("The number of open requirements: ", len(getOpenFeatures(jira)))
 	print ("The number of requirements in progress: ", len(getOpenInProgressFeatures(jira)))
-	# testing: print (jira.issue("TIKA-2232").fields.assignee)
+	print ("The number of closed or resolved issues before an issue has started being implemented: ", getNumRequirementsBeforeThis(jira, 'TIKA-2016'))
+	
+		''' Playing:
+	  numTemp = jira.search_issues("project=TIKA and issueType=\'New Feature\' and status WAS NOT \'In Progress\' DURING (\'2017/01/01\', \'2017/8/7\')", maxResults=1000)
+	  hihih = jira.search_issues("project=TIKA and issueType=\'New Feature\' AND status CHANGED FROM \'Open\' TO \'In Progress\'", maxResults=MAX_RESULTS)
+	'''
 
 if __name__ == "__main__":
 	main()
