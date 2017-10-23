a81a11e4df619659289c3b23b05338513d190c8a
leopangchan <leopangchan@gmail.com>
2017-10-23 15:00:25 -0700

    add the recontructed repo

--- PERILS-Yiupang/httpRequest.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import Request, urlopen
-import credentials # this file contains personal credentials on github, it's not being committed on purpose.
-
-def requestByGitAPI(url):
-  request = Request(url)
-  request.add_header("Authorization", "token %s" % credentials.personal_access_tokens)
-  response = urlopen(request)
-  return response.read()
