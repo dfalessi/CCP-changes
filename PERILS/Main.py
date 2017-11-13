e4d1bfa4618bb986a8f64e3665303b4dea4debed
leopangchan <leopangchan@gmail.com>
2017-11-12 16:12:44 -0800

    add more status for projects

--- PERILS/Main.py
+++ PERILS/Main.py
@@ -47,13 +47,16 @@ def __getValidGitRepo(eachRepo):
   else:
     return False
 
-def updateProjectStatus(projectName):
+'''
+Updates status of a project
+'''
+def updateProjectStatus(projectName, status):
   projectData = None
   with open ("./Dataset/project-names.json", 'r+') as outfile:
     projectData = json.load(outfile)
     outfile.seek(0)
     outfile.truncate()
-    projectData[projectName] = True
+    projectData[projectName] = status
     json.dump(projectData, outfile, indent=4, sort_keys=True)
     outfile.close()
 
@@ -80,15 +83,17 @@ def main():
           print("Validated git repo in Main = ", goodGitRepo)
           urlInfo["repository"].append(goodGitRepo)
           localRepos.append(config.LOCAL_REPO.format(projectName))
-      updateProjectStatus(projectName)
+      updateStatus = None
       # either of jira or git repo is not available.
       if urlInfo["jira"] is None:
         print("Missing JIRA database.")
+        updateProjectStatus(projectName, "NoJIRA")
         continue
       elif len(urlInfo["repository"]) == 0:
         print("Missing Git repositories.")
+        updateProjectStatus(projectName, "NoGit")
         continue
-
+      updateProjectStatus(projectName, True)
       proj = ProjectApache(urlInfo["jira"], urlInfo["repository"], localRepos)
       csvRows.append(proj.csvRows)
       Utility.prettyPrintJSON(Utility.getCurrentRateLimit())
